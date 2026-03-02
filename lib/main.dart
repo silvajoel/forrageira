f57a92d4584dac5da0596e5c382f67af062c3127
unknown <silvajoel06@gmail.com>
2026-03-01 21:48:33 -0300

    feat: Add auth_check for login persistence.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:forrageira/widgets/auth_check.dart';
+import 'package:provider/provider.dart';
 import 'package:forrageira/screens/analysis_screen.dart';
 import 'package:forrageira/screens/forgot_password_screen.dart';
 import 'package:forrageira/screens/profile_screen.dart';
@@ -7,23 +9,23 @@ import 'package:forrageira/screens/register_screen.dart';
 import 'package:forrageira/screens/reset_password_screen.dart';
 import 'package:forrageira/screens/submit_analysis_screen.dart';
 import 'package:forrageira/screens/main_screen.dart';
+import 'package:forrageira/services/auth_service.dart';
 import 'firebase_options.dart';
-import 'screens/login_screen.dart';
-import 'screens/home_screen.dart';
 import 'theme/app_theme.dart';
 
-
-/// It is necessary to use Firebase.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AuthService(),
+      child: const MyApp(),
+    ),
   );
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -35,9 +37,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Forrageira',
       theme: appTheme,
-      initialRoute: '/',
+
+      /// Controle de autenticação
+      home: AuthCheck(),
+
       routes: {
-        '/': (context) => const LoginScreen(),
         '/home': (context) => const MainScreen(),
         '/register': (context) => const RegisterScreen(),
         '/forgotpassword': (context) => const ForgotPasswordScreen(),
